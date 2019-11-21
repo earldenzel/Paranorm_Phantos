da40ac5c7f46914f0569ebc355e4e6fe78db6989
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2019-11-21 10:43:29 -0500

    Updated mansion 2

--- Scripts/scenes/mansion_2.ts
+++ Scripts/scenes/mansion_2.ts
@@ -5,11 +5,31 @@ module scenes {
         constructor() {
             // hasDoorTop, hasDoorBot, hasDoorLeft, hasDoorRight
             super(false, true, false, false);
+            this.hasProjectileShooters = true;
             this.Start();
         }
 
         // Methods
         public Start(): void {
+
+            this.enemies[0] = new objects.ShootingFLower(new math.Vec2(170, 300));
+            this.enemies[0].attackPower = 1;
+            this.enemies[1] = new objects.ShootingFLower(new math.Vec2(370, 200));
+            this.enemies[1].attackPower = 1;
+            this.enemies[2] = new objects.ShootingFLower(new math.Vec2(170, 520));
+            this.enemies[2].attackPower = 1;
+            this.enemies[3] = new objects.ShootingFLower(new math.Vec2(370, 420));
+            this.enemies[3].attackPower = 1;
+
+            this.obstacles[0] = new objects.Barriers(managers.Game.mansion_TextureAtlas, "Mansion_Desk_s_ontal");
+            this.obstacles[0].SetPosition(new math.Vec2(180, 380));
+            this.obstacles[1] = new objects.Barriers(managers.Game.mansion_TextureAtlas, "Mansion_Desk_s_ontal");
+            this.obstacles[1].SetPosition(new math.Vec2(380, 280));
+            this.obstacles[2] = new objects.Barriers(managers.Game.mansion_TextureAtlas, "Mansion_Desk_s_ontal");
+            this.obstacles[2].SetPosition(new math.Vec2(180, 600));
+            this.obstacles[3] = new objects.Barriers(managers.Game.mansion_TextureAtlas, "Mansion_Desk_s_ontal");
+            this.obstacles[3].SetPosition(new math.Vec2(380, 500));
+
             managers.Game.player.sceneOnBot = config.Scene.MANSION_6;
             super.Start();
             this.playerInfo.PlayerLocation = new math.Vec2(46,28);
